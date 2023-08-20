97ec3248592eb377a6fcc59631a3a42362ad692d
macosta2187 <76165163+macosta2187@users.noreply.github.com>
2023-08-20 00:34:45 -0300

    Inserta Camionero

--- app/Http/Controllers/CamionerosController.php
+++ app/Http/Controllers/CamionerosController.php
@@ -3,8 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Camionero;
+
 
 class CamionerosController extends Controller
 {
-    //
+    public function Insertar(Request $request)
+    {
+
+        $camionero = new Camionero;
+        $camionero->libreta_vencimiento = $request->input('libreta_vencimiento');
+        $camionero->id_chofer = $request->input('id_chofer');        
+        $camionero->save();
+
+        return response()->json(['message' => 'Camionero creado exitosamente'], 200);
+
+    }
 }
