3e43a99c576cbf41abefbdae08ef69b844b95f19
macosta2187 <76165163+macosta2187@users.noreply.github.com>
2023-08-20 22:58:01 -0300

    Insertar Camiones

--- app/Http/Controllers/CamionesController.php
+++ app/Http/Controllers/CamionesController.php
@@ -3,8 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Camiones;
 
 class CamionesController extends Controller
 {
-    //
+    public function Insertar(Request $request)
+    {
+
+        $camiones = new Camiones;
+        $camiones->matricula = $request->input('matricula');
+        $camiones->marca = $request->input('marca');
+        $camiones->modelo = $request->input('modelo');
+        $camiones->capacidad_peso = $request->input('capacidad_peso');        
+        $camiones->save();
+
+        return response()->json(['message' => 'Camion creado exitosamente'], 200);
+
+    }
 }
